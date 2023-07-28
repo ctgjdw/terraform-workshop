31d080708af9318180f76e6a1e94bdeef8cd2b60
Goh Chun Teck <gohchunteck@hotmail.com>
2023-07-28 14:27:56 +0800

    remove newline

--- control/init-docker.sh
+++ control/init-docker.sh
@@ -3,8 +3,7 @@
 DO_TOKEN=$(cat /root/do_token)
 rm /root/do_token
 
-until docker-machine version
-do
+until docker-machine version; do
     echo "Waiting for docker-machine to be installed..."
     sleep 1
 done
@@ -12,18 +11,18 @@ done
 echo "Creating Docker droplet"
 
 docker-machine create \
-        -d digitalocean \
-        --digitalocean-access-token  $DO_TOKEN \
-        --digitalocean-image ubuntu-18-04-x64  \
-        --digitalocean-region sgp1 \
-        --digitalocean-backups=false \
-        --engine-install-url "https://releases.rancher.com/install-docker/19.03.9.sh" \
-        docker-nginx
+    -d digitalocean \
+    --digitalocean-access-token $DO_TOKEN \
+    --digitalocean-image ubuntu-18-04-x64 \
+    --digitalocean-region sgp1 \
+    --digitalocean-backups=false \
+    --engine-install-url "https://releases.rancher.com/install-docker/19.03.9.sh" \
+    docker-nginx
 
 echo "Docker droplet provisioned..."
 
 echo "Setting env variables for terraform..."
 
-echo -n "export SSH_P_KEY=/root/.ssh/id_rsa" >> /root/.bashrc
-docker-machine env docker-nginx | grep DOCKER_HOST >> /root/.bashrc
-docker-machine env docker-nginx | grep DOCKER_CERT_PATH >> /root/.bashrc
+echo "export SSH_P_KEY=/root/.ssh/id_rsa" >>/root/.bashrc
+docker-machine env docker-nginx | grep DOCKER_HOST >>/root/.bashrc
+docker-machine env docker-nginx | grep DOCKER_CERT_PATH >>/root/.bashrc
